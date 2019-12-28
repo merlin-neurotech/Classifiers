d21db4a6425b13fc7d59b92ba6c442cdb5791ca1
Awni <awnyaltabaa@gmail.com>
2019-12-28 14:13:37 +0300

    added ensemble method and renamed some things.
    added ensemble function for synthesis of epochs and their labels in one line.
    renamed windows to epochs for consistency.
    some edits to documentations.

--- Generic/preprocessing.py
+++ Generic/preprocessing.py
@@ -3,33 +3,32 @@
     Usage Example: [TODO]
 """
 
-#TODO: should "windows" be called "epochs"?
-
 import numpy as np
 from scipy import stats
 
-def create_windows(data, window_size, inter_window_interval):
+
+def epoch(signal, window_size, inter_window_interval):
     """
-    Creates overlapping windows of EEG data from a single recording.
+    Creates overlapping windows/epochs of EEG data from a single recording.
     
     Arguments:
-        data: array of timeseries EEG data
+        signal: array of timeseries EEG data of shape [n_samples, n_channels]
         window_size(int): desired size of each window in number of samples
-        inter_window_interval(int): interval between each window in number of samples
+        inter_window_interval(int): interval between each window in number of samples (measured start to start)
         
     Returns: 
-        numpy array object with the windows along its first dimension
+        numpy array object with the epochs along its first dimension
     """
     
     #Calculate the number of valid windows of the specified size which can be retrieved from data
-    #Explanation: the max overflow possible is window_size, so we check how many 
-    num_windows = 1 + (len(data) - window_size) // inter_window_interval
+    num_windows = 1 + (len(signal) - window_size) // inter_window_interval
     
-    windows = []
+    epochs = []
     for i in range(num_windows):
-        windows.append(data[i*inter_window_interval:i*inter_window_interval + window_size])
+        epochs.append(signal[i*inter_window_interval:i*inter_window_interval + window_size])
     
-    return np.array(windows)
+    return np.array(epochs)
+
 
 def labels_from_timestamps(timestamps, sampling_rate, length):
     """takes an array containing timestamps (as floats) and
@@ -61,42 +60,46 @@ def labels_from_timestamps(timestamps, sampling_rate, length):
     return np.array(labels)
 
 
-def label_windows(labels, window_size, inter_window_interval, label_method):
-    """create labels for individual windows of EEG data based on the label_method.
-    For now, the "containment" and "count" label_methods assume binary labels.
+def label_epochs(labels, window_size, inter_window_interval, label_method):
+    """create labels for individual eoicgs of EEG data based on the label_method.
+    Note: For now, the "containment" and "count" label_methods assume binary labels.
 
     Arguments:
-        labels: an integer array indicating a class for each time bin 
+        labels: an integer array indicating a class for each sample measurement
         window_size(int): size of each window in number of samples (matches window_size in EEG data)
         inter_window_interval(int): interval between each window in number of samples (matches inter_window_interval in EEG data)
-        label_method(str): method of consolidating labels contained in window into a single label:
-            "containment": whether a positive label is contained in the window,
-            "count": the count of positive labels in the window,
-            "mode": the most common label in the window
+        label_method(str): method of consolidating labels contained in epoch into a single label:
+            "containment": whether a positive label is contained in the epoch,
+            "count": the count of positive labels in the epoch,
+            "mode": the most common label in the epoch
     
     Returns:
-        an array with a label correponding to each window
+        a numpy array with a label correponding to each epoch
     """
 
-    windows = create_windows(labels, window_size, inter_window_interval)
+    # epoch the labels themselves so each epoch contains a label at each sample measurement
+    epochs = epoch(labels, window_size, inter_window_interval)
 
+    #if a positive label [1] is occurs in the epoch, give epoch positive label
     if label_method == "containment":
-        window_labels = [int(1 in window) for window in windows]
+        epoch_labels = [int(1 in epoch) for epoch in epochs]
 
     elif label_method == "count":
-        # counts the number of occurences of the positive label (1)
+        # counts the number of occurences of the positive label [1]]
         #TODO: maybe have have the label to be counted specified by the function
         #perhaps these should be broken up to different functions?
-        window_labels = [window.count(1) for window in windows]
+        epoch_labels = [epoch.count(1) for epoch in epochs]
 
     elif label_method == "mode":
-        #choose the most common occurence in the window and default to 0 if multiple exist
-        window_labels = [stats.mode(window)[0][0] for window in windows]
+        #choose the most common label occurence in the epoch and default to the smallest if multiple exist
+        epoch_labels = [stats.mode(epoch)[0][0] for epoch in epochs]
 
 
-    return np.array(window_labels)
+    return np.array(epoch_labels)
 
-def label_windows_from_timestamps(timestamps, sampling_rate, length, window_size, inter_window_interval, label_method="containment", raw_labels=False):
+
+def label_epochs_from_timestamps(timestamps, sampling_rate, length, window_size, inter_window_interval,
+                                 label_method="containment"):
     """
     Directly creates labels for individual windows of EEG data from timestamps of events.
 
@@ -116,7 +119,36 @@ def label_windows_from_timestamps(timestamps, sampling_rate, length, window_size
     """
     labels = labels_from_timestamps(timestamps, sampling_rate, length)
 
-    window_labels = label_windows(labels, window_size, inter_window_interval, label_method)
+    epoch_labels = label_epochs(labels, window_size, inter_window_interval, label_method)
 
-    if not raw_labels: return window_labels
-    else: return labels
+    return epoch_labels
+
+
+def epoch_and_label(data, sampling_rate, timestamps, window_size, inter_window_interval, label_method="containment"):
+    """
+    Epochs a signal (single EEG recording) and labels each epoch using timestamps of events
+    and a chosen labelling method.
+    
+    Arguments:
+        data: array of timeseries EEG data of shape [n_samples, n_channels]
+        timestamps: an array of floats containing the timestamps for each event (units must match sampling_rate).
+        sampling_rate(float): the sampling rate of the EEG data.
+        window_size(int): desired size of each window in units of time (matches sampling_rate)
+        inter_window_interval(int): interval between each window in units of time (measured start to start; matches sampling_rate)
+        label_method(str): method of consolidating labels contained in window into a single label:
+            "containment": whether a positive label is contained in the window,
+            "count": the count of positive labels in the window,
+            "mode": the most common label in the window
+
+        
+    Returns: 
+        epochs: an array of the epochs with shape [n_epochs, n_channels]
+        labels: an array of the labels corresponding to each epoch of shape [n_epochs, ]
+    """
+    
+    epochs = epoch(data, int(window_size*sampling_rate), int(inter_window_interval*sampling_rate))
+    labels = label_epochs_from_timestamps(timestamps, sampling_rate, len(data), 
+                                          int(window_size*sampling_rate), int(inter_window_interval*sampling_rate), 
+                                          label_method=label_method)
+    
+    return epochs, labels
